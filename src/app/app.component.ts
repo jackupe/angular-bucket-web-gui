aec636bc0880c6e266ea7c0bbca91fd6158a9ed2
JPietrzak2 <jacek.pietrzak@external.t-mobile.pl>
2024-04-08 11:07:12 +0200

    init

---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+import { FileUploadComponent } from './components/file-upload/file-upload.component';
+
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet, FileUploadComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'web-gui';
+}
